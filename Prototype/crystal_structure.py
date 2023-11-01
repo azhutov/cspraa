672660a472d9f6f1b93c49907975aab49179d560
Artem Zhutov <artem.zhutov@gmail.com>
2023-11-01 08:39:43 -0400

    update encoding Format

--- Prototype/crystal_structure.py
+++ Prototype/crystal_structure.py
@@ -1,123 +1,75 @@
-import warnings
-import re
+from typing import List, Tuple
 
 
 class CrystalStructure:
-    def __init__(self, vertices_count, species_count):
+    def __init__(self, vertices_count: int, species_count: int, dimension: int = 3):
         self.vertices_count = vertices_count
         self.species_count = species_count
-        self.positions = []
-        self.potentials = [[] for _ in range(vertices_count)]
-        self.interactions = {}
+        self.dimension = dimension
+        self.positions: List[Tuple[float, ...]] = []
+        self.potentials: List[List[float]] = [[0.0 for _ in range(species_count)] for _ in range(vertices_count)]
+        self.interactions: List[Tuple[int, int, List[float]]] = []
 
-    def add_position(self, *args):
-        # Single position mode
-        if len(args) == 3:
-            x, y, z = args
-            if len(self.positions) < self.vertices_count:
-                self.positions.append((x, y, z))
-            else:
-                warnings.warn(
-                    f"Position limit reached ({self.vertices_count} positions allowed). Position ({x}, {y}, {z}) was not added.")
-        # Bulk mode
-        elif len(args) == 1 and isinstance(args[0], list):
-            for position in args[0]:
-                if len(self.positions) < self.vertices_count:
-                    self.positions.append(position)
-                else:
-                    x, y, z = position
-                    warnings.warn(
-                        f"Position limit reached ({self.vertices_count} positions allowed). Position ({x}, {y}, {z}) and subsequent positions were not added.")
-                    break
-        else:
-            raise ValueError("Invalid arguments for add_position")
+    def add_position(self, *coords: float) -> None:
+        if len(coords) != self.dimension:
+            raise ValueError(f"Expected {self.dimension} coordinates, got {len(coords)}")
+        if len(self.positions) < self.vertices_count:
+            self.positions.append(coords)
 
-    def set_potential(self, *args):
-        # Single potential mode
-        if len(args) == 2:
-            position_idx, potentials = args
-            if 0 <= position_idx < self.vertices_count:
-                self.potentials[position_idx] = potentials
-        # Bulk mode
-        elif len(args) == 1 and isinstance(args[0], list):
-            for position_idx, potentials in args[0]:
-                if 0 <= position_idx < self.vertices_count:
-                    self.potentials[position_idx] = potentials
-        else:
-            raise ValueError("Invalid arguments for set_potential")
+    def set_potential(self, position_idx: int, potentials: List[float]) -> None:
+        if 0 <= position_idx < self.vertices_count:
+            self.potentials[position_idx] = potentials
 
-    def set_interaction(self, *args):
-        # Single interaction mode
-        if len(args) == 3:
-            position_idx1, position_idx2, interactions = args
-            if (0 <= position_idx1 < self.vertices_count and 0 <= position_idx2 < self.vertices_count):
-                key = (position_idx1, position_idx2)
-                self.interactions[key] = interactions
-        # Bulk mode
-        elif len(args) == 1 and isinstance(args[0], list):
-            for position_idx1, position_idx2, interactions in args[0]:
-                if (0 <= position_idx1 < self.vertices_count and 0 <= position_idx2 < self.vertices_count):
-                    key = (position_idx1, position_idx2)
-                    self.interactions[key] = interactions
-        else:
-            raise ValueError("Invalid arguments for set_interaction")
+    def add_interaction(self, position_idx1: int, position_idx2: int, interactions: List[float]) -> None:
+        if (0 <= position_idx1 < self.vertices_count and 0 <= position_idx2 < self.vertices_count):
+            self.interactions.append((position_idx1, position_idx2, interactions))
 
-    def encode(self):
-        encoded = []
-        encoded.append(f"{self.vertices_count} {self.species_count}")
+    @staticmethod
+    def from_string(encoded_str: str) -> 'CrystalStructure':
+        lines = encoded_str.strip().split("\n")
+        vertices_count, species_count = map(int, lines[0].split())
+        dimension = int(lines[1])
+        structure = CrystalStructure(vertices_count, species_count, dimension)
 
-        for i, (x, y, z) in enumerate(self.positions):
-            encoded.append(f"#{i}: {x}, {y}, {z}")
+        for line in lines[2:2+vertices_count]:
+            coords = list(map(float, line.split()))
+            structure.add_position(*coords)
 
-        for i in range(self.vertices_count):
-            potentials = self.potentials[i]
-            encoded_potentials = ", ".join([f"({j}, {w:.1f})" for j, w in potentials])
-            encoded.append(f"#{i}: {encoded_potentials}")
+        for idx, line in enumerate(lines[2+vertices_count:2+2*vertices_count]):
+            potentials = list(map(float, line.split()))
+            structure.set_potential(idx, potentials)
 
-        for (position_idx1, position_idx2), interactions in self.interactions.items():
-            encoded_interactions = ", ".join([f"({i}, {j}, {w:.1f})" for i, j, w in interactions])
-            encoded.append(f"#{position_idx1}, #{position_idx2}: {encoded_interactions}")
-
-        return "\n".join(encoded)
-
-    @classmethod
-    def decode(cls, encoded_str):
-        # Extract vertices_count and species_count
-        header_pattern = re.compile(r"^\s*(\d+) (\d+)")
-        match = header_pattern.search(encoded_str)
-        if not match:
-            raise ValueError("Failed to decode header information.")
-        vertices_count, species_count = map(int, match.groups())
-
-        structure = cls(vertices_count, species_count)
-
-        # Extract positions
-        position_pattern = re.compile(r"#(\d+): (-?\d+\.\d+), (-?\d+\.\d+), (-?\d+\.\d+)")
-        for match in position_pattern.finditer(encoded_str):
-            x, y, z = map(float, match.groups()[1:])
-            structure.add_position(x, y, z)
-
-        # Extract potentials
-        potential_pattern = re.compile(r"#(\d+): ((\(\d+, -?\d+\.\d+\),? ?)+)")
-        for match in potential_pattern.finditer(encoded_str):
-            potentials = [(int(p[0]), float(p[1])) for p in re.findall(r"\((\d+), (-?\d+\.\d+)\)", match.group(2))]
-            structure.set_potential(int(match.group(1)), potentials)
-
-        # Extract interactions
-        interaction_pattern = re.compile(r"#(\d+), #(\d+): ((\(\d+, \d+, -?\d+\.\d+\),? ?)+)")
-        for match in interaction_pattern.finditer(encoded_str):
-            interactions = [(int(i[0]), int(i[1]), float(i[2]))
-                            for i in re.findall(r"\((\d+), (\d+), (-?\d+\.\d+)\)", match.group(3))]
-            structure.set_interaction(int(match.group(1)), int(match.group(2)), interactions)
+        for line in lines[3+2*vertices_count:]:
+            parts = list(map(float, line.split()))
+            position_idx1, position_idx2 = int(parts[0]), int(parts[1])
+            interactions = parts[2:]
+            structure.add_interaction(position_idx1, position_idx2, interactions)
 
         return structure
 
-    def write_to_file(self, file_path):
-        with open(file_path, 'w') as file:
-            file.write(self.encode())
+    def to_string(self) -> str:
+        encoded = []
+        encoded.append(f"{self.vertices_count} {self.species_count}")
+        encoded.append(str(self.dimension))
+        for position in self.positions:
+            encoded.append(" ".join(map(str, position)))
 
-    @classmethod
-    def read_from_file(cls, file_path):
+        for potentials in self.potentials:
+            encoded.append(" ".join(map(str, potentials)))
+
+        encoded.append(str(len(self.interactions)))
+        for position_idx1, position_idx2, interactions in self.interactions:
+            interaction_str = " ".join(map(str, interactions))
+            encoded.append(f"{position_idx1} {position_idx2} {interaction_str}")
+
+        return "\n".join(encoded)
+
+    @staticmethod
+    def from_file(file_path: str) -> 'CrystalStructure':
         with open(file_path, 'r') as file:
             encoded_str = file.read()
-        return cls.decode(encoded_str)
+        return CrystalStructure.from_string(encoded_str)
+
+    def to_file(self, file_path: str) -> None:
+        with open(file_path, 'w') as file:
+            file.write(self.to_string())
