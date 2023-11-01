c6a4f18aac160ea1072e040d8bdc1a8d1b99757d
Soroush Khoubyarian <40778228+soroush1379@users.noreply.github.com>
2023-11-01 19:39:55 -0400

    arbitrary connectivi qpu generator empty -SK

---
+++ Prototype/ArbitraryConnectivityQPUGenerator.py
@@ -0,0 +1,7 @@
+from Prototype.GenericQPUGenerator import GenericQPUGenerator
+
+class ArbitraryConnectivityQPUGenerator(GenericQPUGenerator):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        pass
