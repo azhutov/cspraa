6a2349a842c9ac23deb9e8a6d2b8f3440eb64c57
Soroush Khoubyarian <40778228+soroush1379@users.noreply.github.com>
2023-11-01 00:20:07 -0400

    moving crystal infrastructure to its own folder

--- classical_solvers/brute_force/main.cpp
+++ classical_solvers/brute_force/main.cpp
@@ -1,18 +1,17 @@
+#include <brute_force_solver.h>
 #include <iostream>
 #include <crystal.h>
-#include <brute_force_solver.h>
 #include <vector>
 
 using namespace std;
 
 int main() {
-    Crystal crystal("../../native_crystal_database/edge.dat");
+    Crystal crystal("../../native_crystal_database/hexagon_binary.dat");
     crystal.load();
 
     BruteForceSolver solver(crystal);
     Solution s = solver.solve();
 
-    cout << "energy: " << s.getEnergy() << endl;
     s.save("./solutions/output.yaml");
 
     return 0;
