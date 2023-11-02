44c124a04f9aa9cb073e21fbbff5897d4fee1642
Soroush Khoubyarian <40778228+soroush1379@users.noreply.github.com>
2023-11-02 18:22:38 -0400

    adding comments to generic qpu generator

--- Prototype/GenericQPUGenerator.py
+++ Prototype/GenericQPUGenerator.py
@@ -16,5 +16,6 @@ class GenericQPUGenerator:
         """Returns a dictionary with the following keys:
             - "positions": A list of tuples (size 2 or 3 depending on crystal dimension) containing the position of the atoms.
             - "detunings": A list of floats containing the detunings corresponding to each atom. This list must  be normalized such that the largest value in the list is 1.
+            - "rydberg_radius": The desired rydberg radius for the algorithm.
         """
         raise NotImplementedError("This is a generic class")
