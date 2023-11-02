136cd5a8813913516d023cb37bc5fdc82197e9a8
Soroush Khoubyarian <40778228+soroush1379@users.noreply.github.com>
2023-11-02 18:55:34 -0400

    updating generic qpu generic class

--- Prototype/GenericQPUGenerator.py
+++ Prototype/GenericQPUGenerator.py
@@ -1,10 +1,37 @@
 from Prototype.crystal_structure import CrystalStructure
+from typing import List, Dict, Tuple
+
+class AtomSpec:
+
+    def __init__(self, position: Tuple, detuning: float, target: int):
+        """_summary_
+
+        Args:
+            position (Tuple): A tuple containing the coordinates of the atom in the QPU.
+            detuning (float): A float determining the relative detuning of the atom, in units of delta_max.
+            target (int): An integer determining the target of the qubit. If the qubit is ancillary, this value is -1.
+        """
+        self.position = position
+        self.detuning = detuning
+        self.target = target
+
+    def getPosition(self):
+        return self.position
+    
+    def getDetuning(self):
+        return self.detuning
+    
+    def setPosition(self, position):
+        self.position = position
+
+    def setDetuning(self, detuning):
+        self.detuning = detuning
 
 class GenericQPUGenerator:
 
     def __init__(self, 
                  crystal: CrystalStructure, 
-                 atomic_min_distance: float):
+                 atomic_min_distance: float = 1):
         self.crystal = crystal
         self.dimension = crystal.dimension
         self.positions = crystal.positions
@@ -12,10 +39,9 @@ class GenericQPUGenerator:
         self.interactions = crystal.interactions
         self.atomic_min_distance = atomic_min_distance
     
-    def convert(self):
+    def convert(self) -> Dict:
         """Returns a dictionary with the following keys:
-            - "positions": A list of tuples (size 2 or 3 depending on crystal dimension) containing the position of the atoms.
-            - "detunings": A list of floats containing the detunings corresponding to each atom. This list must  be normalized such that the largest value in the list is 1.
+            - "atom_specs": A list of atom specs.
             - "rydberg_radius": The desired rydberg radius for the algorithm.
         """
         raise NotImplementedError("This is a generic class")
