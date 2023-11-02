881c0761b796cdec106382b011fbb6c3efd7792b
Soroush Khoubyarian <40778228+soroush1379@users.noreply.github.com>
2023-11-02 17:43:11 -0400

    adding comments -SK

--- Prototype/ArbitraryConnectivityQPUGenerator.py
+++ Prototype/ArbitraryConnectivityQPUGenerator.py
@@ -4,4 +4,10 @@ class ArbitraryConnectivityQPUGenerator(GenericQPUGenerator):
 
     def __init__(self, *args):
         super().__init__(*args)
-        pass
+
+    def convert(self):
+        """Returns a dictionary with the following keys:
+            - "positions": A list of tuples (size 2 or 3 depending on crystal dimension) containing the position of the atoms.
+            - "detunings": A list of floats containing the detunings corresponding to each atom. This list must  be normalized such that the largest value in the list is 1.
+        """
+        
