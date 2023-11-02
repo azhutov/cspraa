04b671b2799e725d5b0fa2aa64a4c7f32ad1631e
Soroush Khoubyarian <40778228+soroush1379@users.noreply.github.com>
2023-11-02 18:53:40 -0400

    minor changes to generic qpu generator

--- Prototype/ArbitraryConnectivityQPUGenerator.py
+++ Prototype/ArbitraryConnectivityQPUGenerator.py
@@ -1,19 +1,27 @@
-from Prototype.GenericQPUGenerator import GenericQPUGenerator
+from Prototype.GenericQPUGenerator import GenericQPUGenerator, AtomSpec
+import numpy as np
 
 class ArbitraryConnectivityQPUGenerator(GenericQPUGenerator):
 
     def __init__(self, 
+                 weights_detuning_fraction: float,
                  copy_gadget_detuning_correction: float,
                  next_nearest_neighbour_detuning_correction: float,
                  *args):
+        self.weights_detuning_fraction = weights_detuning_fraction
         self.copy_gadget_detuning_correction = copy_gadget_detuning_correction
         self.next_nearest_neighbour_detuning_correction = next_nearest_neighbour_detuning_correction
         super().__init__(*args)
 
     def convert(self):
         """Returns a dictionary with the following keys:
-            - "positions": A list of tuples (size 2 or 3 depending on crystal dimension) containing the position of the atoms.
-            - "detunings": A list of floats containing the detunings corresponding to each atom. This list must  be normalized such that the largest value in the list is 1.
+            - "atom_specs": A list of atom specs.
             - "rydberg_radius": The desired rydberg radius for the algorithm.
         """
+        atom_specs = []
+        rydberg_radius = self.atomic_min_distance * np.sqrt(5) / 4
+
+        # for i in range()
+        # self.add_target_line()
+
         
